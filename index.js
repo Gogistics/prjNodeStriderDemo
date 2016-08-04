7c650b474ad15a2b18482b980b1ad4d4dc38c95a
Alan <invisible_alan@hotmail.com>
2016-08-04 14:16:53 -0700

    update test case

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello Strider');
+  res.send('Welcome to Strider');
 });
  
 app.listen(process.env.PORT || 5000);
