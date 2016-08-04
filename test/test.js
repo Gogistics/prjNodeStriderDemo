eb03457a3b6460a8f3c9b8d6b104ab76efad9cfa
Alan <invisible_alan@hotmail.com>
2016-08-04 14:09:00 -0700

    update test example

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('Hello Strider...', done);
+    request(app).get('/').expect('Hello Strider', done);
   });
 });
