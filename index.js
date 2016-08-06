c062090c990a9621145fc3677bdcea7ffbc2c1eb
Alan Tai <gogistics.tw@gmail.com>
2016-08-06 13:59:05 -0700

    update test case

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Strider/Nodejs Demo');
 });
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ first_name: 'Alan', last_name: "Tai" });
+});
  
 app.listen(process.env.PORT || 5000);
  
--- test/test.js
+++ test/test.js
@@ -6,3 +6,12 @@ describe('GET /', function() {
     request(app).get('/').expect('Strider/Nodejs Demo', done);
   });
 });
+
+describe('GET /user', function(){
+  it('respond with json', function(done){
+    request(app).get('/user')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200, done);
+  });
+});
